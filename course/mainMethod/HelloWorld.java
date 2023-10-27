910147e849593d1d0de171daec2879973195c251
Danté <dante.whyte@hubject.com>
2023-10-27 10:00:26 +0200

    Written the first part of introduction to the Project

---
+++ course/mainMethod/HelloWorld.java
@@ -0,0 +1,9 @@
+package course.mainMethod;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+    String intro = "Hello World!!";
+
+    System.out.println(intro);
+    }
+}
