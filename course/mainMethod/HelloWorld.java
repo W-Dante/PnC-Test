58457962b409f732c44dab338dc4afba9570e92d
Danté <dante.whyte@hubject.com>
2023-10-29 08:02:42 +0100

    Written the first part of introduction to the Project

--- course/mainMethod/HelloWorld.java
+++ course/mainMethod/HelloWorld.java
@@ -4,6 +4,15 @@ public class HelloWorld {
     public static void main(String[] args) {
     String intro = "Hello World!!";
 
+
     System.out.println(intro);
+    //System.out.println(Author);
     }
+    public class Author{
+        Author Dante = new Author();
+        static String name = "Danté";
+        int age = 23;
+        String nationality = "British";
+    }
+
 }
