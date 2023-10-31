2a85135e0a35f004bb5529e38c9b9e1ea4d0c7c0
Danté <dante.whyte@hubject.com>
2023-10-31 08:29:55 +0100

    Written the Author class

---
+++ course/mainMethod/Author.java
@@ -0,0 +1,14 @@
+package course.mainMethod;
+
+public class Author {
+    public String name;
+    public int age;
+    public String nationality;
+
+    public Author(String name, int age, String nationality){
+        super();
+        this.name = name;
+        this.age = age;
+        this.nationality = nationality;
+    }
+}
