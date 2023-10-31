1428e5387028a3afec15f6dfbfae7fdcf7d440eb
Danté <dante.whyte@hubject.com>
2023-10-31 08:52:24 +0100

    Update CreatedBy.java
    Added the method to ensure presentation

--- course/mainMethod/CreatedBy.java
+++ course/mainMethod/CreatedBy.java
@@ -3,5 +3,6 @@ package course.mainMethod;
 public class CreatedBy {
     public static void main(String[] args) {
         Author Dante = new Author("Dante", 23, "British");
+        Dante.sleep();
     }
 }
