8a3ba8a2b3486f91d71c860e47958b24307d161f
Danté Whyte <dante.whyte@hubject.com>
2023-11-09 12:00:49 +0100

    Updating the Java classes

--- course/mainMethod/HelloWorld.java
+++ course/mainMethod/HelloWorld.java
@@ -1,10 +1,15 @@
 package course.mainMethod;
 
+
 public class HelloWorld {
     public static void main(String[] args) {
+        Author Dante = new Author("Danté", 23, "British","Male");
     String intro = "Hello World!!";
 
 
     System.out.println(intro);
+    System.out.println("\n\n\n");
+
+    System.out.println("Created by " + Dante.name);
     }
 }
