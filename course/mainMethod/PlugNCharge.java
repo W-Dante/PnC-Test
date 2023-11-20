c25a339a173ece2769696398ace4c50725b7d252
Danté Whyte <dante.whyte@hubject.com>
2023-11-20 13:51:41 +0100

    Added a PnC Page

---
+++ course/mainMethod/PlugNCharge.java
@@ -0,0 +1,42 @@
+package course.mainMethod;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class PlugNCharge {
+    public static void main(String[] args) {
+        Scanner Keyboard = new Scanner(System.in);
+        String onboarding;
+        PnC();
+        System.out.print("Hubject currently has onboarding organisations. In which section would you like " +
+                "to view (OEM / EMP / CPO) : ");
+        onboarding = Keyboard.next();
+        if (Objects.equals(onboarding, "OEM")){
+            System.out.println("\nScania, Renault & Polestar");
+        if (Objects.equals(onboarding, "EMP")){
+            System.out.println("\nBosch, Plugsurfing, Virta, Monta, Carge");
+        if (Objects.equals(onboarding, "CPO")) {
+            System.out.println("\nBlink, Zunger Charging Hero, Mer, Sdey, Compleo, TotalEnergies, EVgo, Allego, Vattenfall");
+        }}}
+        else{
+            System.out.println("This is an invalid input.");
+            System.exit(0);
+        }
+
+    }
+
+    public static void PnC(){
+        System.out.println("Plug&Charge enables fully automatic charging processes." +
+                "\nWith automatic authentication technology between the electric vehicle and the charging station,\n" +
+                "drivers can connect and charge their EV immediately - without apps or RFID cards.\n\n");
+
+        String[] OEMs= {"Volkswagen", "Audi", "Porsche", "Skoda", "Cupra", "Lucid", "Mercedes-Benz", "MAN",
+                "Genesis","Hyundai","BMW Group", "Kia"};
+        String[] EMPs = {"Digital Charging Solutions", "Elli", "Shell Recharge Solutions", "Northe", "Porsche",
+        "Deftpower","Ford", "Octopus Electroverse", "EVWAY", "Zunder", "AutoSense", "Stations-e"};
+        String[] CPOs = {"Ionity", "BP Pulse", "Electrify America","BayWa", "Shell Recharge Solutions", "EVWAY",
+                "Porsche", "Smatric", "Stations-e", "Charzin", "EKOEN"};
+
+
+    }
+}
