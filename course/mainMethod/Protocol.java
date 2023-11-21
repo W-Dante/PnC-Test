be54389bf6a8d52c8469b9f732ef0bc6c7fca736
Danté Whyte <dante.whyte@hubject.com>
2023-11-21 11:25:49 +0100

    Created a Protocol class and fixed some bugs in PlugNCharge class

---
+++ course/mainMethod/Protocol.java
@@ -0,0 +1,8 @@
+package course.mainMethod;
+
+public class Protocol {
+    public static void main(String[] args) {
+        System.out.println("\nISO15118 -> Road Vehicles - Vehicle to Grid Communication Interface\n\n");
+    }
+}
+
