1cd720a70b733690444826c632b22ac6caa0c04d
Danté Whyte <dante.whyte@hubject.com>
2023-11-23 11:57:15 +0100

    Created a class to access the link to Hubject's Plug and Charge website

---
+++ course/mainMethod/OpenPnCLink.java
@@ -0,0 +1,21 @@
+package course.mainMethod;
+
+import java.awt.Desktop;
+import java.net.URI;
+
+public class OpenPnCLink {
+    public static void main(String[] args) {
+        if (!Desktop.isDesktopSupported()) {
+            System.out.println("Desktop is not supported");
+            return;
+        }
+
+        Desktop desktop = Desktop.getDesktop();
+        try {
+            URI uri = new URI("https://www.hubject.com/openplugncharge");
+            desktop.browse(uri);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
